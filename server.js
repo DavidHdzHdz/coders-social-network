7134a15cedbf705a181a5e7cbdc14668e670dec5
David <david.hernandez.hernandez.contractor@bbva.com>
2019-06-14 00:44:32 -0500

    add login and register routes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 // middleware
 app.use(express.json({ extended: false }));
+// cors headers
+app.use((_, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 // root api menssage and routes
 app.get('/', (req, res) => res.send('API running'));
 app.use('/api/auth', require('./routes/auth'));
